1bb2aa9136f9b2c3ae865f95e20302ffbe85929f
qsbit <965499224@qq.com>
2021-04-17 18:24:00 +0800

    Site updated: 2021-04-17 18:23:35

---
+++ js/weather.js
@@ -0,0 +1,48 @@
+// 天气插件
+// WIDGET = {
+//     CONFIG: {
+//         "modules": "01234",
+//         "background": 5,
+//         "tmpColor": "ffffff",
+//         "tmpSize": 16,
+//         "cityColor": "ffffff",
+//         "citySize": 16,
+//         "aqiSize": 16,
+//         "weatherIconSize": 24,
+//         "alertIconSize": 18,
+//         "padding": "10px 10px 10px 10px",
+//         "shadow": "0",
+//         "language": "zh",
+//         "borderRadius": 5,
+//         "fixed": "true",
+//         "vertical": "middle",
+//         "horizontal": "center",
+//         "left": "210",
+//         "top": 10,
+//         "key": "nt6eBj5RG1"
+//     }
+// }
+
+
+
+WIDGET = {
+  CONFIG: {
+    "modules": "01234",
+    "background": 1,
+    "tmpColor": "FFFFFF",
+    "tmpSize": 16,
+    "cityColor": "FFFFFF",
+    "citySize": 16,
+    "aqiSize": 16,
+    "weatherIconSize": 24,
+    "alertIconSize": 18,
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": 5,
+    "fixed": "false",
+    "vertical": "middle",
+    "horizontal": "center",
+    "key": "nt6eBj5RG1"
+  }
+}
